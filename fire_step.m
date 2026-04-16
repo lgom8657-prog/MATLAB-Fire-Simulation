6b9ce878a78f8100cb589c7ec2e8ce80759b88f8
Antoni <aoli3147@giant.cos.edu>
2026-04-16 11:37:23 -0700

    Add files via upload
    Initial fire and fire step functions

---
+++ fire_step.m
@@ -0,0 +1,55 @@
+function fire = fire_step(fire, params);
+
+% Function fire_step updates the intensity of fire one step at a time
+
+% Burning cells spread to its NSEW neighbors
+% cells naturally decay by a small amount
+% values are clamped between [0, 1]
+
+% Inputs:
+
+%  fire 
+% .intensity -  a GridSize x GridSize matrix with values in [0(no fire),1(fire)]
+
+% Params 
+% SpreadRate - the rate at which fire spreads to its neighbors
+% DecayRate - how much the fire decays naturally after each step
+% GridSize - number of rows and columns
+
+% Outputs:
+
+% fire - the updated struct with new intensity
+
+
+CurrentGrid = params.GridSize;
+OldIntensity = fire.intensity;
+
+for row = 1:CurrentGrid
+    for col = 1:CurrentGrid
+        if OldIntensity(row,col) > 0
+            % North
+            if row > 1
+                fire.intensity(row-1,col) = min(fire.intensity(row-1,col) + params.SpreadRate * OldIntensity(row,col), 1);
+            end 
+            % South
+            if row < CurrentGrid
+                fire.intensity(row + 1,col) = min(fire.intensity(row + 1, col) + params.SpreadRate * OldIntensity(row,col), 1);
+            end
+            % West
+            if col > 1
+                fire.intensity(row,col - 1) = min(fire.intensity(row,col - 1) + params.SpreadRate * OldIntensity(row,col), 1);
+            end
+            % East
+            if col < CurrentGrid
+                fire.intensity(row,col+1) = min(fire.intensity(row, col + 1) + params.SpreadRate * OldIntensity(row,col), 1);
+            end
+        end
+    end
+end
+
+fire.intensity = fire.intensity - params.DecayRate;
+
+% Clamps values to just [0, 1]
+fire.intensity = max(fire.intensity, 0);
+fire.intensity = min(fire.intensity, 1);
+end
---
+++ init_fire.m
@@ -0,0 +1,29 @@
+function fire = init_fire(params)
+
+% init_fire function creates the intital fire struct
+
+% function creates a GridSize by GridSize matrix of zeros, then places a
+% choosen amount of fire hotspots at random locations with an intensity of
+% 1.
+
+% Inputs:
+% params
+% GridSize - number of rows and columns
+% RndFire - number of randomly places fire hotspots
+
+% Outputs:
+% fire
+% intensity - a GridSize x GridSize matrix with values in [0(no fire),1(fire)]
+
+
+% Intial Matrix Grid
+fire.intensity = zeros(params.GridSize, params.GridSize);
+
+% Places Intital Hotspots in random locations
+
+for i = 1:params.RndFire
+    row = randi(params.GridSize);
+    col = randi(params.GridSize);
+    fire.intensity(row, col) = 1;
+end
+end
